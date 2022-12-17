3dc0685ac74dc3a19a43b79463b9d13670b96395
Loki99911 <nikita_kresik@i.ua>
2022-12-17 13:52:26 +0200

    Add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from 'path/to/pages/Home';
+import About from 'path/to/pages/About';
+import Products from 'path/to/pages/Products';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
+};
